902ace94f0a7c9c7819feed55cb672a0d78f1a23
Aaron <aaron.tognoli@mail.polimi.it>
2023-03-23 18:08:02 +0100

    Added simple result test

--- src/main/java/AstarBstar/Main.java
+++ src/main/java/AstarBstar/Main.java
@@ -1,5 +1,7 @@
 package AstarBstar;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import de.learnlib.algorithms.lstar.dfa.ClassicLStarDFA;
 import de.learnlib.algorithms.lstar.dfa.ClassicLStarDFABuilder;
 
@@ -10,10 +12,21 @@ import de.learnlib.oracle.equivalence.DFAWMethodEQOracle;
 import de.learnlib.util.Experiment;
 import de.learnlib.util.statistics.SimpleProfiler;
 import net.automatalib.automata.fsa.DFA;
+import net.automatalib.automata.fsa.impl.compact.CompactDFA;
+import net.automatalib.commons.smartcollections.ArrayUtil;
+import net.automatalib.graphs.Graph;
 import net.automatalib.serialization.dot.GraphDOT;
 import net.automatalib.visualization.Visualization;
+import net.automatalib.visualization.dot.DOT;
+import net.automatalib.visualization.dot.DOTComponent;
 
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -45,6 +58,22 @@ public class Main {
 
         // get learned model
         DFA<?, Character> result = experiment.getFinalHypothesis();
+        //try system
+        System.out.println(result.accepts(toCharacterArray("aaa")));
+        System.out.println(result.accepts(toCharacterArray("aabb")));
+        System.out.println(result.accepts(toCharacterArray("bbb")));
+        System.out.println(result.accepts(toCharacterArray("")));
+        System.out.println(result.accepts(toCharacterArray("bbbaa")));
+        //TODO find way to save result
+        /*
+        CompactDFA<Character> toJson = (CompactDFA<Character>) result;
+        Gson gson = new GsonBuilder().setPrettyPrinting().create();
+        //save result to Json
+        try (FileWriter writer = new FileWriter("./myDFA.json")) {
+            gson.toJson(toJson, writer);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
 
         // report results
         System.out.println("-------------------------------------------------------");
@@ -67,9 +96,26 @@ public class Main {
         GraphDOT.write(result, TestDriver.SIGMA, System.out); // may throw IOException!
 
 
+
         Visualization.visualize(result, TestDriver.SIGMA);
 
         System.out.println("-------------------------------------------------------");
 
+
+
+
     }
+
+
+    public static List<Character> toCharacterArray(String s)
+    {
+        List<Character> out = new ArrayList<>();
+        for(char c : s.toCharArray())
+        {
+            out.add(c);
+        }
+        return out;
+    }
+
 }
+
